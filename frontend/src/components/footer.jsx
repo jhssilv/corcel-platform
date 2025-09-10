2c624fb8c780a034a938bc50ada3d65fe53d8264
GabrielPazinatto <gabrielrpazinatto@gmail.com>
2025-09-10 14:34:36 -0300

    Updated repo url

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -6,7 +6,7 @@ import '../styles/footer.css'
 function Footer() {
     return (
         <section>
-            <a href="https://github.com/dev-joseh" target="_blank" rel="noopener nofererrer">
+            <a href="https://github.com/jhssilv/corcel-platform" target="_blank" rel="noopener nofererrer">
                 <img className="logo" src={github_logo} alt="github logo" />
             </a>
         </section>
