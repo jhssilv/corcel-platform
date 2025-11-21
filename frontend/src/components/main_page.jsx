d01dd5a8a0bfe1131c54a440db34439431e96ba3
gabrielpazinatto <gabrielrpazinatto@gmail.com>
2025-11-21 09:45:45 -0300

    Added top bar to main page and slightly changed styling

--- frontend/src/components/main_page.jsx
+++ frontend/src/components/main_page.jsx
@@ -1,64 +1,57 @@
-import { useState, useEffect, useCallback, useContext } from 'react';
-import '../styles/main_page.css';
+"use client"
 
-import EssaySelector from './essay_selector.jsx';
-import EssayDisplay from './essay_display.jsx';
-import AuthContext from './auth_context.jsx';
-import DownloadDialog from './download_dialog.jsx';
-import DownloadButton from './download_button.jsx';
+import { useState, useEffect, useCallback, useContext } from "react"
+import "../styles/main_page.css"
 
-import downloadTexts from './api/download_texts.jsx';
-import { getTextById, getNormalizationsByText } from './api/api_functions.jsx';
+import EssaySelector from "./essay_selector.jsx"
+import EssayDisplay from "./essay_display.jsx"
+import AuthContext from "./auth_context.jsx"
+import DownloadDialog from "./download_dialog.jsx"
+import TopBar from "./top_bar.jsx"
+
+import downloadTexts from "./api/download_texts.jsx"
+import { getTextById, getNormalizationsByText } from "./api/api_functions.jsx"
 
 // MAIN PAGE COMPONENT \\
 
 function MainPage() {
-    const [selectedEssay, setSelectedEssay] = useState(null);
-    const [currentText, setCurrentText] = useState(null);
-    const [showDownloadDialog, setShowDownloadDialog] = useState(false);
+  const [selectedEssay, setSelectedEssay] = useState(null)
+  const [currentText, setCurrentText] = useState(null)
+  const [showDownloadDialog, setShowDownloadDialog] = useState(false)
 
-    const { userId } = useContext(AuthContext);
+  const { userId } = useContext(AuthContext)
 
-    const fetchEssay = useCallback(async () => {
-        const text = await getTextById(selectedEssay.value, userId);
-        const normalizations = await getNormalizationsByText(selectedEssay.value, userId);
-        
-        text.corrections = normalizations;
-        setCurrentText(text);
-    }, [selectedEssay, userId]);
+  const fetchEssay = useCallback(async () => {
+    const text = await getTextById(selectedEssay.value, userId)
+    const normalizations = await getNormalizationsByText(selectedEssay.value, userId)
 
-    useEffect(() => {
-        if (selectedEssay) fetchEssay();
-    }, [selectedEssay, fetchEssay]);
+    text.corrections = normalizations
+    setCurrentText(text)
+  }, [selectedEssay, userId])
 
+  useEffect(() => {
+    if (selectedEssay) fetchEssay()
+  }, [selectedEssay, fetchEssay])
 
-    return (
-        <section>
-            <h1>CorCel 🐴</h1>
-            <h2>Plataforma de Normalização Ortográfica</h2>
+  return (
+    <section className="main-page-section">
+      <TopBar onDownloadClick={() => setShowDownloadDialog(true)} />
 
-            <div> 
-                <DownloadButton onClick={() => setShowDownloadDialog(true)}>Baixar Textos
-                    <span role="img" aria-label="download">⬇️</span>
-                </DownloadButton>
-                <EssaySelector 
-                    selectedEssay={selectedEssay}
-                    setSelectedEssay={setSelectedEssay}
-                />
-            </div>
+      <h2>Buscador de Textos</h2>
 
-            <EssayDisplay 
-                essay={currentText}
-                refreshEssay={fetchEssay}
-            />
+      <div>
+        <EssaySelector selectedEssay={selectedEssay} setSelectedEssay={setSelectedEssay} />
+      </div>
 
-            <DownloadDialog
-                show={showDownloadDialog}
-                onClose={() => setShowDownloadDialog(false)}
-                onDownload={(useBrackets) => downloadTexts(useBrackets)}
-            />
-        </section>
-    );
+      <EssayDisplay essay={currentText} refreshEssay={fetchEssay} />
+
+      <DownloadDialog
+        show={showDownloadDialog}
+        onClose={() => setShowDownloadDialog(false)}
+        onDownload={(useBrackets) => downloadTexts(useBrackets)}
+      />
+    </section>
+  )
 }
 
-export default MainPage;
+export default MainPage
