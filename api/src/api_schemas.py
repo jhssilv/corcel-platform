ea3b488592ab651bdd011da0d392d58cd43d147a
GabrielPazinatto <gabrielrpazinatto@gmail.com>
2025-11-21 22:16:16 -0300

    Added schema to request report

--- api/src/api_schemas.py
+++ api/src/api_schemas.py
@@ -90,3 +90,7 @@ class DownloadRequest(BaseModel):
     """Schema for the download request body."""
     text_ids: List[int] = Field(..., example=[1, 2, 3], description="List of text IDs to download.")
     use_tags: bool = Field(False, description="Whether to use tags in the normalized tokens.")
+
+class ReportRequest(BaseModel):
+    """Schema for the report request body."""
+    text_ids: List[int] = Field(..., example=[1, 2, 3], description="List of text IDs to generate the report for.")
