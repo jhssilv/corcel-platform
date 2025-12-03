b0d188c4bf53c3e281c1259a29eaaeaf394aa347
gabrielpazinatto <gabrielrpazinatto@gmail.com>
2025-12-03 16:43:15 -0300

    Removed unused folder

--- backend/app/extensions.py
+++
@@ -1,5 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from celery import Celery
-
-db = SQLAlchemy()
-celery = Celery()
--- backend/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-celery -A tasks worker --loglevel=info &
-flask run --host=0.0.0.0 --port=5000
