a643cc625a41becd6d7e5922ee5817abac3d5cc2
GabrielPazinatto <gabrielrpazinatto@gmail.com>
2025-12-19 14:55:28 -0300

    added bcrypt and jwt objects

--- api/app/extensions.py
+++ api/app/extensions.py
@@ -1,5 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
 from celery import Celery
+from flask_jwt_extended import JWTManager
+from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
 celery = Celery()
+jwt = JWTManager()
+bcrypt = Bcrypt()
