9fd35ae9d0a721432b0e4277e9376f8ea7cb6bec
GabrielPazinatto <gabrielrpazinatto@gmail.com>
2025-12-19 15:01:12 -0300

    changed db access object to global extensions.db

--- api/app/generate_report.py
+++ api/app/generate_report.py
@@ -1,28 +1,26 @@
-from app.database.queries import get_original_text_tokens_by_id, get_normalizations_by_text, get_text_by_id, get_username_by_id
-from app.database.connection import get_db_session
 
 import csv
 import io
 
-CONTEXT_WINDOW = 5
+from app.database.queries import get_original_text_tokens_by_id, get_normalizations_by_text, get_text_by_id, get_username_by_id
+from app.extensions import db
 
-session = get_db_session()
+CONTEXT_WINDOW = 5
 
 def generate_report(user_id:int, text_ids:list[int]):
     output = io.StringIO()
     writer = csv.writer(output, delimiter=';')
-    username = get_username_by_id(session, user_id)
+    username = get_username_by_id(db, user_id)
     
     # Write CSV header
     output.write('\ufeff')  # BOM for UTF-8
     writer.writerow(['Text ID', 'User', 'Previous Tokens', 'Word', 'Subsequent Tokens', 'Normalization'])
     
-    
     for text_id in text_ids:
-        tokens = get_original_text_tokens_by_id(session, text_id)        
+        tokens = get_original_text_tokens_by_id(db, text_id)        
         tokens.sort(key=lambda token: token.position)
-        normalizations = get_normalizations_by_text(session, text_id, user_id)
-        text_sourcefilename = get_text_by_id(session, text_id, user_id)['source_file_name']
+        normalizations = get_normalizations_by_text(db, text_id, user_id)
+        text_sourcefilename = get_text_by_id(db, text_id, user_id)['source_file_name']
 
         for norm in normalizations:
             prev_tokens = tokens[max(0, norm.start_index - CONTEXT_WINDOW):norm.start_index]
