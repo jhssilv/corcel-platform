d88a4bd1b10ed8192f573cac28598718ca63f8bb
GabrielPazinatto <gabrielrpazinatto@gmail.com>
2025-12-24 12:29:10 -0300

    Added search info icon

--- frontend/src/components/EssaySelector.jsx
+++ frontend/src/components/EssaySelector.jsx
@@ -150,18 +150,28 @@ const EssaySelector = ({
         <form className="essay-selector-container">
             {/* Essay Dropdown */}
             <div className="selector-main-search">
-                <DropdownSelect
-                    title="ID do Texto"
-                    options={filteredEssays}
-                    selectedValues={selectedEssay}
-                    onChange={handleEssayChange}
-                    isMulti={false}
-                    filterOption={null}
-                    inputValue={essayInputValue}
-                    onInputChange={handleEssayInputChange}
-                    controlShouldRenderValue={false}
-                    blurInputOnSelect={false}
-                />
+                <div className="search-with-info">
+                    <DropdownSelect
+                        title="ID do Texto"
+                        options={filteredEssays}
+                        selectedValues={selectedEssay}
+                        onChange={handleEssayChange}
+                        isMulti={false}
+                        filterOption={null}
+                        inputValue={essayInputValue}
+                        onInputChange={handleEssayInputChange}
+                        controlShouldRenderValue={false}
+                        blurInputOnSelect={false}
+                    />
+                    <div className="info-tooltip-container">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="info-icon">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
+                        </svg>
+                        <div className="tooltip-text">
+                            Digite o id do texto para buscar. Exemplo: "2015 n4" encontrará "2015_n4_12345.txt".
+                        </div>
+                    </div>
+                </div>
             </div>
 
             <div className="selector-filters-grid">
