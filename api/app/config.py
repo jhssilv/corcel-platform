52ab0375dafdc509e8c58b861d9cf73e38e6970a
GabrielPazinatto <gabrielrpazinatto@gmail.com>
2026-03-27 20:04:13 -0300

    enforces secret key definition on .env

--- api/app/config.py
+++ api/app/config.py
@@ -9,13 +9,14 @@ class Config:
     CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL', 'redis://localhost:6379/0')
     CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND', 'redis://localhost:6379/0')
     
-    SECRET_KEY = os.getenv('SECRET_KEY', 'dev_key')
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    
+    if SECRET_KEY is None:
+        raise ValueError("SECRET_KEY environment variable is not set.")
     
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     JWT_TOKEN_LOCATION = ['cookies']
-    
-    SECRET_KEY = os.getenv('SECRET_KEY', 'dev_key')
     
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=24)
     
