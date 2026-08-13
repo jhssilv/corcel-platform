61e7947cb11a628160d75634ca45ce8b56050c5b
GabrielPazinatto <gabrielrpazinatto@gmail.com>
2026-08-12 22:33:09 -0300

    simplified scheduled localbackup functionality

--- backups/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Making initial backup..."
-/backup_script.sh
-
-# Start the cron service in the foreground.
-# The 'exec "$@"' executes the command passed by the CMD in the Dockerfile.
-echo "Starting cron daemon for scheduled backups..."
-exec "$@"
